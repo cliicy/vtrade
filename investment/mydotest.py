1bc744a6d331b4b733f6b6658b8310eb0c30524e
luojianjun <luojianjun@yingu.com>
2018-08-30 11:38:36 +0800

    add argv to control the times of fetching the order information

--- investment/mydotest.py
+++ investment/mydotest.py
@@ -1,20 +1,22 @@
-import datetime,time
-from api.handlers.fcoin_transaction_handler import FcoinTransactionHandler
-from enums import Symbol,ContractType, Platform,OKEX_SYMBOL_LIST,OKEX_FUTURE_SYMBOL_LIST
+import time,sys
+from investment.api.handlers.fcoin_transaction_handler import FcoinTransactionHandler
 
 
 if __name__ == '__main__':
+    steps = int(sys.argv[1])
     # for test : query id information 10 times to check the consuming time
     fh = FcoinTransactionHandler()
     # 获取成交历史列表中的已经成交的订单信息
-    stime_begin = time.strftime('%H:%M:%S', time.localtime())
-    second1 = stime_begin.split(':')[2]
-    print("开始时间：%s" %second1)
-    for index in range(10):
+    # stime_begin = time.strftime('%H:%M:%S', time.localtime())
+    # second1 = stime_begin.split(':')[2]
+    stime_begin = time.time()
+    print("开始时间：%s" %stime_begin)
+    for index in range(steps):
         orders = fh.get_orders('xrpusdt', 'filled')
 
-    stime_end = time.strftime('%H:%M:%S', time.localtime())
-    second2 = stime_end.split(':')[2]
-    print("结束时间：%s" % second2)
-    time_space = float(second2) - float(second1)
-    print('查询订单10次所需时间：%s' % time_space)
+    # stime_end = time.strftime('%H:%M:%S', time.localtime())
+    # second2 = stime_end.split(':')[2]
+    stime_end = time.time()
+    print("结束时间：%s" % stime_end)
+    time_space = float(stime_end) - float(stime_begin)
+    print('查询订单 %s 次所需时间：%s' % (steps,time_space))
